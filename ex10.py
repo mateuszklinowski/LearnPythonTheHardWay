8b31fc24a5e030da8fc289c7224680c91f0a3f68
Mateusz Klinowski <mateusz.klinowski@tsh.io>
2019-04-06 10:54:55 +0200

    Exercise 10 - What Was That?

---
+++ ex10.py
@@ -0,0 +1,28 @@
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\n on a line"
+backslash = "I have \\ in my \\ string"
+
+
+long = """
+I will do alist
+\t* Cat foog
+\t* Fishes
+\t* Catnik \n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash)
+print(long)
+
+print("Bell: \a AAA")
+print("Backspace: \b BBB")
+print("formfeed: \f FFF")
+print("carrige return: \r RRR")
+
+# single quote also wotks
+print('''
+kappa
+kapppa
+kappppa
+''')
