2d374f5a51dee80a31504b7a6fafe88fe28690fc
Mateusz Klinowski <mateusz.klinowski@tsh.io>
2019-04-06 11:44:26 +0200

    Exercise 11 Asking Questions

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("give age", end=' ')
+age = input()
+
+print(f"Age is: {age}")
+
+
+print("type of input ", type(age))
+
+number = int(input("Enter a number: "))
+
+print("Number is ", number, " type is ", type(number))
