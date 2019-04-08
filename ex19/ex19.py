18a8e0345d91c42f49d6af1288b53c3e49dc25e8
Mateusz Klinowski <mateusz.klinowski@tsh.io>
2019-04-08 08:17:39 +0200

    Exercise 19 - Functions and Variables

---
+++ ex19/ex19.py
@@ -0,0 +1,13 @@
+def print_args(arg1, arg2):
+    print(f"{arg1}, {arg2}")
+
+print_args(20, 30)
+
+#with variables
+one = 1
+two = 2
+
+print_args(one,two)
+
+#with math inside
+print_args(10+2, two + one + 100)
