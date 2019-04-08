24be690d6405f628c3a0360e70f2a7b95ef2a9f3
Mateusz Klinowski <mateusz.klinowski@tsh.io>
2019-04-08 20:48:30 +0200

    Exercise 21 - FUNCTIONS CAN RETURN SOMETHING

---
+++ ex21/ex21.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+age = add(30,5)
+height = subtract(78,4)
+weight = multiply(90,2)
+iq = divide(120,2)
+
+print(f"{age}, {height}, {weight}, {iq}")
